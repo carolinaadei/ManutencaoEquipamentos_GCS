d1c5adf02c3d07ef5ea3031e0758a0521083b991
Cauab12 <cauabemficaa051@gmail.com>
2025-04-19 16:16:45 -0300

    atualizando classe ControleManutencao

--- ControleManutencao.java
+++ ControleManutencao.java
@@ -21,5 +21,25 @@ public class ControleManutencao {
                 System.out.println("--------------------------------------");
             }
         }
+
+        boolean encontrou = false;
+
+        for (Manutencao manutencao : manutencoes) {
+            if (manutencao.isEmManutencao()) {
+                Equipamento equipamento = manutencao.getEquipamento();
+                Funcionario responsavel = equipamento.getFuncionarioResponsavel();
+
+                System.out.println("Equipamento: " + equipamento.getNomeCurto());
+                System.out.println("Status da Manutenção: " + manutencao.getStatus());
+                System.out.println("Funcionário Responsável: " + responsavel.getNome());
+                System.out.println("--------------------------------------");
+
+                encontrou = true;
+            }
+        }
+
+        if (!encontrou) {
+            System.out.println("Nenhum equipamento está em manutenção no momento.\n");
+        }
     }
 }
